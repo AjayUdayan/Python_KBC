3289c75ff679cf6115a0b6bf1c8324757dab059d
Ajay Udayan <44021625+AjayUdayan@users.noreply.github.com>
2019-05-31 16:53:29 +0530

    Add files via upload

---
+++ KBC1.py
@@ -0,0 +1,8 @@
+questions = ["What is the capital of telangana?,Which is the smallest state in India?,What is the capital of Himachal Pradesh?,Which is the most populated country in the world?,Raipur is the capital of which state of India?"]
+
+first_options = ["Jaipur,Maharastra, USA,Chhattisgarh"]
+second _options = ["Thiruvananthapuram,Goa,Pakistan,uttrakhand"]
+third _options = ["Chennai,Bihar,China,Harayana"]
+fourth_options = ["Hydarabad,Rajesthan,India,Asam"]
+all_options = ["first_options,second _options,third _options,fourth_options"]
+ans_key  = [3,1,2,0]  
